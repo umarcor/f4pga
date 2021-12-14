814dcf6c19d7bcd1413cbc79b76740743ac0ad57
Unai Martinez-Corral <umartinezcorral@antmicro.com>
2021-12-14 04:49:34 +0100

    ci: merge deploy statements into 'deploy-push.sh'; rename to 'deploy.sh'
    Signed-off-by: Unai Martinez-Corral <umartinezcorral@antmicro.com>

--- .github/deploy.sh
+++ .github/deploy.sh
@@ -2,7 +2,20 @@
 
 set -e
 
+git config --global user.name SymbiFlow
+git config --global user.mail robot@mith.ro
+git config --global push.default simple
+
 GITHUB_USER=$(echo $GITHUB_REPOSITORY | sed -e's-/.*--')
+if [ "$GITHUB_USER" == "SymbiFlow" ]; then
+  git clone https://${GH_TOKEN}@github.com/SymbiFlow/symbiflow.github.io --branch master build
+else
+  git clone https://${GH_TOKEN}@github.com/$GITHUB_REPOSITORY --branch gh-pages build
+fi
+git status
+
+cp -a /tmp/website-build/* build
+
 GIT_DESCRIBE=$(git describe --tags)
 
 cd build
